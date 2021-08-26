448724d5ef4261e45d57b030afbfb9035ffb466b
yjmm10 <yjmm10@yeah.net>
2021-08-26 15:30:03 +0800

    fix label bug

--- tools/autoLabel.py
+++ tools/autoLabel.py
@@ -2,11 +2,13 @@ import json
 import os
 import shutil
 from pathlib import Path
+import numpy as np
+import matplotlib.pyplot as plt
 
 # 自动标注的条件是假设文字大小一致，根据字符长度进行判断，除了公司名称采用直接的数字作为标签，其他标签采用自定义并在该表中体现
 # 5表示非公司名称的字符个数对应的实际标签
-OvalTable = {5: "印章5", 6: "印章6", 15: "中编码", 13: "下编码"}
-CircleTable = {13:"印章编码",5:"质检专用章",6:"法定代表人章"}
+OvalTable = {15: "c15",13: "v13", 6: "t6", 5: "t5"}
+CircleTable = {13: "v13", 5: "t5", 6: "t6"}
 
 # 找到所有涉及到的关键字
 def findFile(rootpath="./tools/Oval",type="Circle"):
@@ -132,37 +134,53 @@ def generalData(root=None,jsonData=None, type='Circle'):
                     continue
                 if index == 0:
                     point = jsonData[str(len(text))]
+                    point = np.around(np.array(point), decimals=1).tolist()
                     point = clockwise(point)
 #                     if point!=None:
 #                         index += 1
-#                         test(point,index)
+#                         plottest(point,index)
                     labelRes.append({"transcription":text,"points":point})
                 else:
                     point = jsonData[typeDict[len(text)]]
+                    point = np.around(np.array(point), decimals=1).tolist()
                     point = clockwise(point)
 #                     if point!=None:
 #                         index += 1
-#                         test(point,index)
+#                         plottest(point,index)
 
                     labelRes.append({"transcription": text, "points": point})
             f.write(f"image/{texts[0]}_{type}.png\t{labelRes}\n")
 
 def mergefiles(root,types):
-
+    res = ''
+    resultDir = os.path.join(root, "label")
+    if not os.path.exists(resultDir):
+        os.makedirs(resultDir)
     resultFile = os.path.join(root, "label.txt")
     if os.path.exists(resultFile):
         os.remove(resultFile)
-    with open(resultFile,"a+") as f:
-        for type in types:
-            typepath = os.path.join(root, type)
-            if not os.path.exists(typepath):
-                continue
-            folder = os.path.join(typepath,'image')
-            dst_folder = os.path.join(root, 'image')
-            copyTree(folder, dst_folder)  # 复制文件夹并覆盖
-            label = os.path.join(typepath,f"label_{type}.txt")
-            txt = open(label,'r').read()
-            f.write(txt)
+
+    for type in types:
+        typepath = os.path.join(root, type)
+        if not os.path.exists(typepath):
+            continue
+        folder = os.path.join(typepath,'image')
+        dst_folder = os.path.join(root, 'image')
+        copyTree(folder, dst_folder)  # 复制文件夹并覆盖
+        label = os.path.join(typepath,f"label_{type}.txt")
+        res += open(label,'r').read()
+    #  生成单个标签文件
+    with open(resultFile, "w") as f:
+        f.write(res)
+    # 一个文件对应一个标签文件
+    resLists = res.split("\n")
+    for resList in resLists:
+        if resList=="":
+            continue
+        filename = resList.split('\t')[0].split('/')[-1].replace(".png",".txt")
+        with open(os.path.join(resultDir,filename),'w') as f:
+            f.write(resList)
+
 
 def clockwise(points:list):
     start, end = points[0], points[-1]
@@ -172,18 +190,45 @@ def clockwise(points:list):
         return points.reverse()
 
 
+# 多项式拟合
+def Curve_Fitting(points,deg):
+    half = int(len(points)/2)
+    x = [i[0] for i in points[:half]]
+    y = [i[1] for i in points[:half]]
 
+    parameter = np.polyfit(x, y, deg)    #拟合deg次多项式
+    p = np.poly1d(parameter)             #拟合deg次多项式
+    aa=''                               #方程拼接  ——————————————————
+    for i in range(deg+1): 
+        bb=round(parameter[i],2)
+        if bb>0:
+            if i==0:
+                bb=str(bb)
+            else:
+                bb='+'+str(bb)
+        else:
+            bb=str(bb)
+        if deg==i:
+            aa=aa+bb
+        else:
+            aa=aa+bb+'x^'+str(deg-i)    #方程拼接  ——————————————————
+    plt.scatter(x, y)     #原始数据散点图
+    plt.plot(x, p(x), color='g')  # 画拟合曲线
+   # plt.text(-1,0,aa,fontdict={'size':'10','color':'b'})
+    plt.legend([aa,round(np.corrcoef(y, p(x))[0,1]**2,2)])   #拼接好的方程和R方放到图例
+    plt.show()
+#    print('曲线方程为：',aa)
+#    print('     r^2为：',round(np.corrcoef(y, p(x))[0,1]**2,2))
+
+from numpy import *
 
 # 测试次序
-def test(points,index):
-    import numpy as np
-    import matplotlib.pyplot as plt
+def plottest(points,index):
+
     # plt.ion()
     x = [i[0] for i in points]
     y = [i[1] for i in points]
     n = np.arange(len(x))
-
-    # fig, ax = plt.subplots()
 
     plt.scatter(x, y, c='r')
 
@@ -195,8 +240,7 @@ def test(points,index):
     plt.close()
 
 
-
-if __name__ == '__main__':
+def main():
     types = ['Oval','Circle','Person']
     workdir = r'.\tools'
     for type in types:
@@ -221,3 +265,12 @@ if __name__ == '__main__':
     if not (os.path.exists(os.path.join(workdir,'image')) and os.path.exists(os.path.join(workdir,'label.txt'))) :
         mergefiles(workdir,types)
 
+
+def test():
+    points = [[64.20433436532508, 216.71826625386998], [53.80612244897959, 196.17346938775512], [46.40816326530613, 178.57142857142858], [43.091836734693885, 157.14285714285714], [42.58163265306122, 135.96938775510205], [48.19387755102041, 114.03061224489797], [57.37755102040816, 95.40816326530613], [69.62244897959184, 79.08163265306122], [83.65306122448979, 64.28571428571429], [99.9795918367347, 54.59183673469388], [121.15306122448979, 47.704081632653065], [142.3265306122449, 44.89795918367347], [165.03061224489795, 44.13265306122449], [181.6122448979592, 48.724489795918366], [202.78571428571428, 57.90816326530612], [218.6020408163265, 69.38775510204081], [230.33673469387753, 82.6530612244898], [244.11224489795921, 100.76530612244898], [252.53061224489795, 118.62244897959184], [256.6122448979592, 137.5], [256.86734693877554, 160.96938775510205], [252.27551020408163, 180.35714285714286], [243.34693877551018, 200.25510204081633], [267.3265306122449, 212.24489795918367], [276.2551020408163, 187.75510204081633], [281.6122448979592, 165.56122448979593], [282.12244897959187, 138.5204081632653], [277.53061224489795, 112.5], [268.60204081632656, 90.56122448979592], [251.51020408163265, 65.05102040816327], [235.69387755102042, 49.48979591836735], [215.28571428571428, 33.673469387755105], [193.60204081632654, 24.23469387755102], [168.3469387755102, 17.602040816326532], [139.26530612244898, 17.602040816326532], [114.01020408163265, 22.448979591836736], [92.07142857142858, 30.867346938775512], [69.87755102040816, 43.62244897959184], [50.744897959183675, 60.96938775510204], [35.183673469387756, 83.16326530612245], [23.448979591836732, 106.88775510204081], [19.112244897959187, 134.18367346938777], [18.602040816326536, 158.41836734693877], [22.683673469387756, 183.67346938775512], [31.867346938775512, 207.90816326530611], [43.46130030959753, 227.55417956656348]]
+    # Curve_Fitting(points,5)
+    pass
+
+if __name__ == '__main__':
+    main()
+    # test()
